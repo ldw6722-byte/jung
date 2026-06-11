f49530125ffb968a5e8932f6ddeb87d32475e56a
leed <ldw6722@gmail.com>
2026-06-11 16:33:56 +0900

    0611

---
+++ src/jung/Main.java
@@ -0,0 +1,23 @@
+//package jung;
+//
+//public class Main {public static void main(String[] args) {
+//    System.out.println("Fun Programming!");
+//}
+//}
+//
+//509
+//public class Main {
+//    public static void main() {
+//        System.out.println("Programming! It's fun.");
+//    }
+//}
+
+//9010
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println(b + " " + a);
+    }
+}
